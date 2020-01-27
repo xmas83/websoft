a798722a61e504719a339cdc18be8d1d197df70d
Mikael Roos <mikael.t.h.roos@gmail.com>
2020-01-27 12:54:42 +0100

    Remove stray comments and code from sample

--- example/javascript/fetch/js/main.js
+++ example/javascript/fetch/js/main.js
@@ -1,28 +1,8 @@
 /**
- * @preserve Made by dbwebb-staff, staff@dbwebb.se
+ * @preserve Made by mos@dbwebb.se
  */
 (function () {
     'use strict';
-
-    //var myContent = document.getElementById('content');
-    // var flagTarget = document.getElementById('flag');
-    // var flagLink = document.getElementById('draw-elfenbenskusten');
-
-    //myContent.innerHTML = '<h3>This is a MEGA template!</h3>';
-
-    // function drawFlagElfenbenskusten()  {
-    //     var flagElfenbenskusten = '<div class="flag elfenbenskusten"><div class="part1"></div>' +
-    //     '<div class="part2"></div></div>';
-    // 
-    //     console.log("Drawing flag");
-    //     flagTarget.innerHTML = flagElfenbenskusten;
-    // }
-    // 
-    // 
-    // flagLink.addEventListener("click", function () {
-    //     console.log("Event for clicking link elfenbenskusten.");
-    //     drawFlagElfenbenskusten();
-    // });
 
 //    fetch('https://api.scb.se/UF0109/v2/skolenhetsregister/sv/kommun/1081')
     fetch('data/1081.json')
